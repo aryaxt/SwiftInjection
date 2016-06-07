3f742c0de4b7e9bac9a62b1846b84a9067c0b041
Aryan <aryaxt@gmail.com>
2016-06-07 11:28:04 -0700

    - Made code more type safe - Don't allow marking structs as singleton

--- SwiftInjection/DIAbstractModule.swift
+++ SwiftInjection/DIAbstractModule.swift
@@ -10,8 +10,17 @@ public class DIAbstractModule {
 	
 	public init() { }
 	
-	public func bind<T>(type: T.Type, named: String? = nil, asSingleton: Bool = false, closure: Void->AnyObject) {
-		DIContainer.instance.bind(type, named: named, asSingleton: asSingleton, closure: closure)
-	}
+    /**
+     Structs are value based and they are not compatible with singleton
+     So we create an overload of bind method that takes Any but doesn't suppport singletons
+     In order to use singleton use the other bind method that takes AnyObject instead of Any
+     */
+    public func bind<T: Any>(type: T.Type, named: String? = nil, closure: Void->T) {
+        DIContainer.instance.bind(type, named: named, asSingleton: false, closure: closure)
+    }
+    
+    public func bind<T: AnyObject>(type: T.Type, named: String? = nil, asSingleton: Bool = false, closure: Void->T) {
+        DIContainer.instance.bind(type, named: named, asSingleton: asSingleton, closure: closure)
+    }
 	
 }
